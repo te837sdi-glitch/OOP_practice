5e5f32cbf864188b294727ed4a61ba2e2d93764f
te837sdi-glitch <te837sdi@gmail.com>
2025-12-06 13:06:46 +0200

    add Student class

---
+++ Praktichna_4/Praktichna_4.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+class Student
+{
+private:
+    int id;
+    string name;
+    string surname;
+    string paternal;
+    int phone;
+public:
+    Student()
+    {
+        id = 0;
+        name = "";
+        surname = "";
+        paternal = "";
+        phone = 0;
+    }
+    Student(int id, string name, string surname, string paternal, int phone)
+    {
+        this->id = id;
+        this->name = name;
+        this->surname = surname;
+        this->paternal = paternal;
+        this->phone = phone;
+    }
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
